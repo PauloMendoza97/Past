5ac0511d1bd0f699ab6831c02ecdb34d2cff1266
Rod - UCSP <Draouch@users.noreply.github.com>
2015-09-10 14:56:44 -0500

    Create pasoporreferencia.cpp

---
+++ pasoporreferencia.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+ 
+int funcion(int &n, int &m);
+ 
+int main() { 
+    int a, b;
+    a = 1; b = 2; 
+    cout << "a, b = " << a << ", " << b << endl;
+    cout << "funcion(a, b) = " << funcion(a, b) << endl; 
+    cout << "a, b = " << a << ", " << b << endl; 
+    return 0; 
+}
+ 
+int funcion(int &n, int &m) {
+    n = n + 2; 
+    m = m - 1; 
+    return n + m; 
+}
