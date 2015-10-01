a77919ffc1592d7c2aca6ed8352d45c09f2dbacd
Rod - UCSP <Draouch@users.noreply.github.com>
2015-10-01 08:12:22 -0500

    Update vector.cpp

--- vector.cpp
+++ vector.cpp
@@ -10,15 +10,24 @@ double Vector::distance(){
 }
 
 void Vector::print(){
-	cout<<"Vector: "<<"("<<start.x<<","<<end.x<<")"<<" a "<<"("<<start.y<<","<<end.y<<")"<<endl;
+	cout<<"Vector: "<<"Inicio ("<<start.x<<","<<end.x<<")"<<" y "<<"Fin ("<<start.y<<","<<end.y<<")"<<endl;
 }
 
 int main() {
+	float p, q, r, s;
 	Vector vec1;
-	vec1.start.x = 3;
-	vec1.start.y = 4;
-	vec1.end.x = 5;
-	vec1.end.y = 6;
+	cout<<"Ingrese start x: ";
+	cin>>p;
+	cout<<"Ingrese end x: ";
+	cin>>q;
+	cout<<"Ingrese start y: ";
+	cin>>r;
+	cout<<"Ingrese end y: ";
+	cin>>s;
+	vec1.start.x = p;
+	vec1.start.y = r;
+	vec1.end.x = q;
+	vec1.end.y = s;
 	vec1.print();
 	double distance = vec1.distance();
 	cout<<distance<<endl;
