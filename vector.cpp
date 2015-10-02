47ea6bd61dd579194353058ec7485ec3e9a053e5
Rod - UCSP <Draouch@users.noreply.github.com>
2015-10-01 23:07:56 -0500

    Update vector.cpp

--- vector.cpp
+++ vector.cpp
@@ -1,34 +1,27 @@
 #include <iostream>
-#include "vector.h"
 #include <math.h>
+#include "vector.h"
 
 using namespace std;
 
 double Vector::distance(){
-	cout<<"Distancia del Vector: ";
-	return sqrt(((pow(end.x-start.x,2))+(pow(end.y-start.y,2))));
+	return sqrt((pow(end.x-start.x,2))+(pow(end.y-start.y,2)));
 }
 
 void Vector::print(){
-	cout<<"Vector: "<<"Inicio ("<<start.x<<","<<end.x<<")"<<" y "<<"Fin ("<<start.y<<","<<end.y<<")"<<endl;
+	cout<<"Vector: "<<"Inicio ("<<start.x<<","<<start.y<<")"<<" y "<<"Fin ("<<end.x<<","<<end.y<<")"<<endl;
 }
 
 int main() {
-	float p, q, r, s;
 	Vector vec1;
-	cout<<"Ingrese start x: ";
-	cin>>p;
-	cout<<"Ingrese end x: ";
-	cin>>q;
-	cout<<"Ingrese start y: ";
-	cin>>r;
-	cout<<"Ingrese end y: ";
-	cin>>s;
-	vec1.start.x = p;
-	vec1.start.y = r;
-	vec1.end.x = q;
-	vec1.end.y = s;
+	cout<<"Ingrese Start X: ";
+	cin>>vec1.start.x;
+	cout<<"Ingrese End X: ";
+	cin>>vec1.start.y;
+	cout<<"Ingrese Start Y: ";
+	cin>>vec1.end.x;
+	cout<<"Ingrese End Y: ";
+	cin>>vec1.end.y;
+	cout<<"La distancia es: "<<vec1.distance()<<endl;
 	vec1.print();
-	double distance = vec1.distance();
-	cout<<distance<<endl;
 }
