b3f1f449f2b377a342588e42cdcdc90aa9abfa1f
Rod - UCSP <Draouch@users.noreply.github.com>
2015-10-07 17:11:10 -0500

    Create problema9.cpp

---
+++ problema9.cpp
@@ -0,0 +1,92 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+	string palabra;
+	char abc[27]="abcdefghijklmnopqrstuvwxyz";
+	cout<<"Ingrese una palabra: ";
+	cin>>palabra;
+	for(int i=0;i<palabra.size();i++){
+		if (palabra[i]==abc[0]){
+			cout<<palabra[i]<<": "<<0<<endl;
+		}
+		if (palabra[i]==abc[1]){
+			cout<<palabra[i]<<": "<<1<<endl;
+		}
+		if (palabra[i]==abc[2]){
+			cout<<palabra[i]<<": "<<2<<endl;
+		}
+		if (palabra[i]==abc[3]){
+			cout<<palabra[i]<<": "<<3<<endl;
+		}
+		if (palabra[i]==abc[4]){
+			cout<<palabra[i]<<": "<<4<<endl;
+		}
+		if (palabra[i]==abc[5]){
+			cout<<palabra[i]<<": "<<5<<endl;
+		}
+		if (palabra[i]==abc[6]){
+			cout<<palabra[i]<<": "<<6<<endl;
+		}
+		if (palabra[i]==abc[7]){
+			cout<<palabra[i]<<": "<<7<<endl;
+		}
+		if (palabra[i]==abc[8]){
+			cout<<palabra[i]<<": "<<8<<endl;
+		}
+		if (palabra[i]==abc[9]){
+			cout<<palabra[i]<<": "<<9<<endl;
+		}
+		if (palabra[i]==abc[10]){
+			cout<<palabra[i]<<": "<<10<<endl;
+		}
+		if (palabra[i]==abc[11]){
+			cout<<palabra[i]<<": "<<11<<endl;
+		}
+		if (palabra[i]==abc[12]){
+			cout<<palabra[i]<<": "<<12<<endl;
+		}
+		if (palabra[i]==abc[13]){
+			cout<<palabra[i]<<": "<<13<<endl;
+		}
+		if (palabra[i]==abc[14]){
+			cout<<palabra[i]<<": "<<14<<endl;
+		}
+		if (palabra[i]==abc[15]){
+			cout<<palabra[i]<<": "<<15<<endl;
+		}
+		if (palabra[i]==abc[16]){
+			cout<<palabra[i]<<": "<<16<<endl;
+		}
+		if (palabra[i]==abc[17]){
+			cout<<palabra[i]<<": "<<17<<endl;
+		}
+		if (palabra[i]==abc[18]){
+			cout<<palabra[i]<<": "<<18<<endl;
+		}
+		if (palabra[i]==abc[19]){
+			cout<<palabra[i]<<": "<<19<<endl;
+		}
+		if (palabra[i]==abc[20]){
+			cout<<palabra[i]<<": "<<20<<endl;
+		}
+		if (palabra[i]==abc[21]){
+			cout<<palabra[i]<<": "<<21<<endl;
+		}
+		if (palabra[i]==abc[22]){
+			cout<<palabra[i]<<": "<<22<<endl;
+		}
+		if (palabra[i]==abc[23]){
+			cout<<palabra[i]<<": "<<23<<endl;
+		}
+		if (palabra[i]==abc[24]){
+			cout<<palabra[i]<<": "<<24<<endl;
+		}
+		if (palabra[i]==abc[25]){
+			cout<<palabra[i]<<": "<<25<<endl;
+		}
+	}
+	return 0;
+}
