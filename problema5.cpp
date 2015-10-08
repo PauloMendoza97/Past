c46d698478fc662c6a9138c433753237e5878f8c
Rod - UCSP <Draouch@users.noreply.github.com>
2015-10-07 19:28:50 -0500

    Update problema5.cpp

--- problema5.cpp
+++ problema5.cpp
@@ -3,15 +3,21 @@
 #include <string>
 
 using namespace std;
+string abrir(string text);
 
-int main () {
+int main(){
 	string text;
+	abrir(text);
+	return 0;
+}
+
+string abrir(string text){
 	ifstream bloc("archivo.txt");
 	if (bloc.is_open()){
 		while(getline(bloc,text)){
-			cout << text <<endl;
+			cout <<text<<endl;
 		}
 		bloc.close();
 	}
-	return 0;
 }
+
