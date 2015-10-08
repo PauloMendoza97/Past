729dfb5ccceee324045f00e47120e9740ce6b695
Rod - UCSP <Draouch@users.noreply.github.com>
2015-10-08 17:36:21 -0500

    Update problema2.cpp

--- problema2.cpp
+++ problema2.cpp
@@ -3,20 +3,24 @@
 #include <iostream>
 
 using namespace std; 
+char sinrep[40]=" "; 
+char palabra[40]; 
+int i=0, j=0; 
 
-int main() 
-{ 
-	char sinrep[40]=" "; 
-	char palabra[40]; 
-	int i=0, j=0; 
-	cout<<"Ingresa una palabra: ";
-	cin>>palabra; 
+void no_repetitions(){
 	while(palabra[i]){ 
 		if(!strchr(sinrep, palabra[i])) 
 			sinrep[j++]=palabra[i]; 
 		i++; 
 	} 
 	sinrep[j]='\0';
+}
+
+int main() 
+{ 
+	cout<<"Ingresa una palabra: ";
+	cin>>palabra;
+	no_repetitions();
 	cout<<"La palabra sin letras repetidas es: "<<sinrep<<endl; 
 	return 0;
 }
