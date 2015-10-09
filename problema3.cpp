545d630161590f770840b2992bcdb4a44eb5087c
Rod - UCSP <Draouch@users.noreply.github.com>
2015-10-08 23:36:46 -0500

    Update problema3.cpp

--- problema3.cpp
+++ problema3.cpp
@@ -1,27 +1,55 @@
 #include <iostream>
 #include <algorithm> 
 
-using namespace std; 
+using namespace std;
+int arr[4000000];
+int n;
 
-int main(){
-	int arr[4000000];
-	int n;
-	cout<<"Ingrese el numero de elementos del array: ";
-	cin>>n;
+void creararr(){
 	arr[1] = arr[n];
 	for(int i = 0; i<n; i++){
-			cout<<"Ingrese el valor "<<"("<<i<<")"<<": ";
-			cin>>arr[i];
+		cout<<"Ingrese el valor "<<"("<<i<<")"<<": ";
+		cin>>arr[i];
 	}
 	cout<<"El arreglo ingresado es: ";
 	for(int i = 0; i<n; i++){
-			cout<<arr[i]<<" ";
+		cout<<arr[i]<<" ";
+	}
+	cout<<"\n";
+}
+
+void ssort(){
+	int temporal;
+	for(int i=0; i<n;i++){
+		for(int j=0;j<n-1;j++){
+			if(arr[j]>arr[j+1]){
+				temporal=arr[j];
+				arr[j]=arr[j+1];
+				arr[j+1]=temporal;
+			}
 		}
-		cout<<"\n";
-	cout<<"El arreglo ordenado es: ";
-	sort(arr,arr+n);
+	}
+	cout<<"El arreglo ordenado por ssort es: ";
+	for(int i=0;i<n;i++){
+		cout<<arr[i]<<" ";
+	}
+} 
+
+void sort(){
+	cout<<"El arreglo ordenado por sort es: ";
+	sort(arr, arr+n);
 	for(int i = 0; i<n; i++){
 		cout<<arr[i]<<" ";
 	}
+}
+
+int main(){
+	cout<<"Ingrese el numero de elementos del array: ";
+	cin>>n;
+	creararr();
+	cout<<"\n";
+	ssort();
+	cout<<"\n";
+	sort();
 	return 0;
 }
