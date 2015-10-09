e30bd0005550ba408cfbe272b93389679ab451ef
Rod - UCSP <Draouch@users.noreply.github.com>
2015-10-09 16:32:38 -0500

    Update problema10.cpp

--- problema10.cpp
+++ problema10.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
 #define n 100
 
-using namespace std; 
+using namespace std;
+int arr[400000];
+int element;
 
-int main(){
-	int arr[400000];
-	int element;
+void arr100(){
 	arr[1] = arr[n];
 	for(int i=0; i<n; i++)
 	{
@@ -15,12 +15,20 @@ int main(){
 	for(int i = 0; i<n; i++){
 		cout<<arr[i]<<" ";
 	}
-	cout<<"\n";
-	cout<<"Elemento a buscar: ";
-	cin>>element;
+}
+
+void buscar(){
 	for(int i=0; i<n; i++){
 		if(arr[i]==element){
 			cout<<"El elemento buscado se encuentra en la posicion "<<i<<"."<<endl;
 		}
 	}
+}
+
+int main(){
+	arr100();
+	cout<<"\n";
+	cout<<"Elemento a buscar: ";
+	cin>>element;
+	buscar();
 }
