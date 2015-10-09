a993c2b0d5385794402f32ac09ec74a21ff7ae94
Rod - UCSP <Draouch@users.noreply.github.com>
2015-10-08 23:44:33 -0500

    Update problema4.cpp

--- problema4.cpp
+++ problema4.cpp
@@ -1,20 +1,19 @@
 #include <iostream>
 
 using namespace std;
-int n;
+int n, fac;
 
 void factorial(){
-	int fac=1;
+	fac=1;
 	for(int i=2; i<=n; i++){
 		fac = fac*i;
 	}
-	cout<<"Es: "<<fac;
 }
 
 int main(){
 	cout<<"Factorial de: ";
 	cin>>n;
 	factorial();
+	cout<<"Es: "<<fac;
 	return 0;
 }
-
