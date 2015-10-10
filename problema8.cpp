e1f1f64accb17f75266544e40ec34a60a30bd0d6
Rod - UCSP <Draouch@users.noreply.github.com>
2015-10-09 21:19:25 -0500

    Update problema8.cpp

--- problema8.cpp
+++ problema8.cpp
@@ -2,17 +2,17 @@
 #include <ctype.h>
 
 using namespace std;
+string palabra;
+
+void mayus(){
+	palabra[0] = toupper(palabra[0]);
+	cout<<"La palabra en mayuscula es: "<<palabra;
+}
 
 int main()
 {
-	string palabra;
-	char cadena[40];
 	cout<<"Ingrese una palabra: "<<endl;
 	cin>>palabra;
-	int i;
-	for(i=0; palabra[i]; i++){
-		palabra[i] = toupper(palabra[i]);
-	}
-	cout<<"La palabra en mayuscula es: "<<palabra;
+	mayus();
 	return 0;
 }
