b3c71e328e5b60dbf5fc718952c0cec5a7d06597
Rod - UCSP <Draouch@users.noreply.github.com>
2015-11-19 00:47:20 -0500

    Update vector.h

--- vector/vector.h
+++ vector/vector.h
@@ -1,9 +1,28 @@
 //vector.h - header file
+#include <iostream>
+#include <math.h>
 #include "point.h"
 
+using namespace std;
+
 class Vector{
+private:
+	Point punto1;
+	Point punto2;
 public:
-	Point start, end;
-	double distance();
-	void print();
+	Vector(){}
+	Vector(double a, double b, double c, double d){
+		punto1.setx(a);
+		punto1.sety(b);
+		punto2.setx(c);
+		punto2.sety(d);
+	}
+	double Distance(){
+		double a = punto1.getx();
+		double b = punto1.gety();
+		double c = punto2.getx();
+		double d = punto2.gety();
+		double distancia = sqrt(pow(a - c, 2) + pow(b - d, 2));
+		return distancia;
+	}
 };
