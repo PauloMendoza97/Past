c019400fccf98e07b0e78939642f168960cabd45
Rod - UCSP <Draouch@users.noreply.github.com>
2015-11-19 00:56:58 -0500

    Update point.h

--- vector/point.h
+++ vector/point.h
@@ -1,6 +1,29 @@
 //point.h - header file
+#include <iostream>
+
+using namespace std;
 
 class Point{
+private:
+	double x1, y1;
 public:
-	double x,y;
+	Point(){x1 = 0;y1 = 0;}
+	Point(double x,double y){
+		x1=x;
+		y1=y;
+	}
+	double getx(){
+		return x1;
+	}
+	double gety(){
+		return y1;
+	}
+	void setx(double x){
+		x1=x;
+		return;
+	}
+	void sety(double y){
+		y1=y;
+		return;
+	}
 };
