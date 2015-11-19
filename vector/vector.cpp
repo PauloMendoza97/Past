fbbd9873c2dfa057846c8671ee1566a1f659538b
Rod - UCSP <Draouch@users.noreply.github.com>
2015-11-19 00:44:25 -0500

    Update vector.cpp

--- vector/vector.cpp
+++ vector/vector.cpp
@@ -1,27 +1,18 @@
-#include <iostream>
-#include <math.h>
 #include "vector.h"
+#include <iostream>
 
 using namespace std;
 
-double Vector::distance(){
-	return sqrt((pow(end.x-start.x,2))+(pow(end.y-start.y,2)));
-}
-
-void Vector::print(){
-	cout<<"Vector: "<<"Inicio ("<<start.x<<","<<start.y<<")"<<" y "<<"Fin ("<<end.x<<","<<end.y<<")"<<endl;
-}
-
-int main() {
-	Vector vec1;
-	cout<<"Ingrese Start X: ";
-	cin>>vec1.start.x;
-	cout<<"Ingrese End X: ";
-	cin>>vec1.start.y;
-	cout<<"Ingrese Start Y: ";
-	cin>>vec1.end.x;
-	cout<<"Ingrese End Y: ";
-	cin>>vec1.end.y;
-	cout<<"La distancia es: "<<vec1.distance()<<endl;
-	vec1.print();
+int main()
+{
+	double a,b,c,d;
+	cout<<"Ingrese las coordenadas del punto de inicio"<<endl;
+	cin>>a>>b;
+	cout<<"Ingrese las coordenadas del punto final"<<endl;
+	cin>>c>>d;
+	Vector Vector1(a,b,c,d);
+	double n=Vector1.Distance();
+	cout<<n<<endl;
+	system("pause");
+	return 0;
 }
