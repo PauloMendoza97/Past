a7f30d176d1cd73d95c98ab474a509f0e6463b93
Rod - UCSP <Draouch@users.noreply.github.com>
2015-11-26 18:50:35 -0500

    Create queue.cpp

---
+++ estructuras/queue.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include "Queue.h"
+
+template<typename T>
+bool Queue1<T>::vacio(){
+	return cola.empty();
+}
+
+template<typename T>
+int Queue1<T>::tamano(){
+	return cola.size();
+}
+
+template<typename T>
+T Queue1<T>::front(){
+	return cola.front();
+}
+
+template<typename T>
+T Queue1<T>::back(){
+	return cola.back();
+}
+
+template<typename T>
+void Queue1<T>::push(T valor){
+	cola.push_back(valor);
+}
+
+template<typename T>
+void Queue1<T>::pop(){
+	cola.erase(cola.begin());
+}
+
+template<typename T>
+void Queue1<T>::print(){
+	for(int i=0; i <cola.size(); i++){
+		cout<<cola[i]<<" ";
+	}
+	cout<<endl;
+}
+
+template<typename T>
+Queue1<T> Queue1<T>::operator+(Queue1<T> col)
+{
+	Queue1<T> tmp;
+	tmp.cola = cola;
+	tmp.cola.insert(tmp.cola.end(), col.cola.begin(), col.cola.end());
+	return tmp;	
+}
+
+template class Queue1<int>;
