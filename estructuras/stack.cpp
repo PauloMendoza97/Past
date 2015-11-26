a6e1d5341b7cae4726b6712427e1f9cf00837e89
Rod - UCSP <Draouch@users.noreply.github.com>
2015-11-26 18:49:33 -0500

    Create stack.cpp

---
+++ estructuras/stack.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include "Stack.h"
+
+template<typename T>
+bool Stack1<T>::vacio()
+{
+	if(pila.empty())
+	{
+		return true;
+	}
+	return false;
+}
+
+template<typename T>
+int Stack1<T>::tamano()
+{
+	return pila.size();
+}
+
+template<typename T>
+T Stack1<T>::top()
+{
+	return pila.back();
+}
+
+template<typename T>
+void Stack1<T>::push(T x)
+{
+	pila.push_back(x);
+}
+
+template<typename T>
+void Stack1<T>::pop()
+{
+	pila.pop_back();
+}
+
+template<typename T>
+void Stack1<T>::print()
+{
+	for(int i=0; i<pila.size(); i++){
+		cout<<pila[i]<<" ";
+	}
+	cout<<endl;
+}
+
+template class Stack1<int>;
